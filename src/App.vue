728fce1b8ed850ee7d58cbf08b4fc287064ac7b3
Darinel Cigarroa <darinelcigarroa97@gmail.com>
2021-09-03 00:02:36 -0500

    creando carrito

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <card-component />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardComponent from "@/components/CardComponent.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import  "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 </style>
